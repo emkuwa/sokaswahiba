a2a543d40351df942cd3a112447e8cfe3e11ee78
emkuwa <mkuwaemmanuel@gmail.com>
2025-04-30 16:01:50 +0300

    Fix: define app with express()

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-app.get('/webhook', (req, res) => {
-  const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "sokaswahiba";
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN || "sokaswahiba";
+
+app.use(express.json());
+
+app.get('/webhook', (req, res) => {
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
@@ -8,11 +14,20 @@ app.get('/webhook', (req, res) => {
   if (mode && token && challenge) {
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
       console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+      return res.status(200).send(challenge);
     } else {
-      res.sendStatus(403); // Token mismatch
+      return res.sendStatus(403);
     }
   } else {
-    res.status(400).send('Missing parameters');
+    return res.status(400).send('Missing query params');
   }
+});
+
+app.post('/webhook', (req, res) => {
+  console.log('Webhook event received:', JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
 });
